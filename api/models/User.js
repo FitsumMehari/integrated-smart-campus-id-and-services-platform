ae48efc9f5a4d701929043e4652e80031c21b6de
FitsumMehari <m.fitsummehari@gmail.com>
2025-03-13 19:38:09 +0300

    add DB Schema for Users

---
+++ api/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: false,
+    },
+    password: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    phone: {
+        type: String,
+        required: false,
+    },
+    userType: {
+        type: String,
+        enum: ["school", "cafe", "gate", "student", "admin"],
+        default: "student",
+        required: false,
+    },
+    generatedOTP: {
+        type: String,
+        required: false,
+    },
+    profilePic: {
+        type: String,
+        required: false,
+    },
+    digitalId: {
+        type: String,
+        required: false,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("user", UserSchema);
