aabd7ea3937952eb5be4d682321d77b3878dfec0
FitsumMehari <m.fitsummehari@gmail.com>
2025-03-13 19:38:04 +0300

    add DB Schema for Notices

---
+++ api/models/Notice.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const NoticeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    owner: {
+        type: String,
+        required: false,
+    },
+    category: {
+        type: String,
+        enum: ["cafe", "gate", "school"],
+        required: false,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("notice", NoticeSchema);
